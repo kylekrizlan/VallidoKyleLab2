89e324dedbac2826090bca15bf882a2b522fbff7
kylekrizlan <kylekrizlan@gmail.com>
2018-09-10 20:51:30 +0800

    Initial commit

---
+++ app/src/main/java/com/vallido/kylelab2/layout.java
@@ -0,0 +1,13 @@
+package com.vallido.kylelab2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.relative);
+    }
+}
